7d157d2fc1654467cd0ef93f9ed83b103e26bfea
Zahidul-Turja <zahidulturja@gmail.com>
2024-05-04 00:57:29 +0600

    Backend: Listing API added and connected to the front

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv").config();
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
+const listingRoutes = require("./routes/listing");
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(express.static("public"));
 
 // ! Routes
 app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
 
 // ! Mongoose setup
 const PORT = 3001;
